5e452cc8105ab08d49a73f95d1081fd7d945cce7
neciporhan <neciporhan@gmail.com>
2020-08-19 12:28:01 -0600

    Print Necip

--- src/day1/Test1.java
+++ src/day1/Test1.java
@@ -1,6 +1,7 @@
 package day1;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
 public class Test1{
+    public static void main(String[] args) {
+        System.out.println("Necip");
+    }
 }
